686d67829a95ba2b3b79fbcfcc7a575426da84cf
Mykyta Berehulia <mykyta.berehulia@gmail.com>
2025-06-09 13:19:16 +0300

    Splits Avrora locations for Kyiv and Lviv

--- avrora-scraper/src/index.js
+++ avrora-scraper/src/index.js
@@ -15,3 +15,11 @@ await writeFile("./data/store_locations.json", json);
 
 const csv = stringify(locations, { header: true });
 await writeFile("./data/store_locations.csv", csv);
+
+const kyiv_locations = locations.filter(({ city }) => city === "Київ");
+const kyiv_csv = stringify(kyiv_locations, { header: true });
+await writeFile("./data/kyiv_store_locations.csv", kyiv_csv);
+
+const lviv_locations = locations.filter(({ city }) => city === "Львів");
+const lviv_csv = stringify(lviv_locations, { header: true });
+await writeFile("./data/lviv_store_locations.csv", lviv_csv);
